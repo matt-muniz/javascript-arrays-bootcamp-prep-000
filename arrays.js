dd84a04cf6a0e052b090d8e0437eed8874d69fd3
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-04 22:48:48 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element){
+  array = [];
+  array[0] = element;
+  console.log(array)
+}
+
+function destructivelyAddElementToBeginningOfArray(element){
+}
+
+addElementToBeginningOfArray([], "hello");
+
+
